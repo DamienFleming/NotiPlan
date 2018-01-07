a21f5f412b7898544526e0b52c4f03f3f49cf4a3
Damien Fleming <damien16fleming@gmail.com>
2018-01-07 14:31:06 -0500

    Added intents to facebook page and email support

--- app/src/main/java/com/damien/notiplan/AboutActivity.java
+++ app/src/main/java/com/damien/notiplan/AboutActivity.java
@@ -1,13 +1,64 @@
 package com.damien.notiplan;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
+
+    public static String FACEBOOK_URL = "https://www.facebook.com/Happiness-is-Contagious-1516588755313659";
+    public static String FACEBOOK_PAGE_ID = "Happiness-is-Contagious-1516588755313659";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Button sendEmail = (Button)findViewById(R.id.emailSupport);
+        Button openFacebook = (Button)findViewById(R.id.facebookSupport);
+
+        sendEmail.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"damien16fleming@gmail.com"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "NotiPlan Support Request");
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+        final Context context = this;
+
+        openFacebook.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent facebookIntent = new Intent(Intent.ACTION_VIEW);
+                String facebookUrl = getFacebookPageIntentURL(context);
+                facebookIntent.setData(Uri.parse(facebookUrl));
+                startActivity(facebookIntent);
+            }
+        });
     }
+
+    public String getFacebookPageIntentURL(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        try {
+            int versionCode = packageManager.getPackageInfo("com.facebook.katana", 0).versionCode;
+            if (versionCode >= 3002850) { //newer versions of fb app
+                return "fb://facewebmodal/f?href=" + FACEBOOK_URL;
+            } else { //older versions of fb app
+                return "fb://page/" + FACEBOOK_PAGE_ID;
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            return FACEBOOK_URL; //normal web url
+        }
+    }
+
 }
